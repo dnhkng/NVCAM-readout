76fd5e2a06025dc5c67e70f9a69a828bc246ed8a
David <dnhkng@gmail.com>
2021-01-27 13:35:20 +0100

    Documentation

--- decoder.vhdl
+++ decoder.vhdl
@@ -6,10 +6,10 @@ use ieee.numeric_std.all ;
 entity decoder is
 	port(	
 		NVCAM_CLK: 		in std_logic;
-		NVCAM_SYNC: 	in std_logic;
-		NVCAM_VSYNC: 	in std_logic;
-		NVCAM_LOCK: 	in std_logic;
-		NVCAM_VALID: 	in std_logic;
+		NVCAM_SYNC: 		in std_logic;
+		NVCAM_VSYNC: 		in std_logic;
+		NVCAM_LOCK: 		in std_logic;
+		NVCAM_VALID: 		in std_logic;
 		lcd_clk: 		out std_logic;
 		lcd_pwm: 		out std_logic;
 		lcd_hsync: 		out std_logic;
@@ -26,14 +26,14 @@ architecture decoder_arch of decoder is
 	
 	-- internal signal declaration 
 	signal row_counter:		unsigned(8 downto 0) := (others => '0');
-	signal line_counter:	unsigned(8 downto 0) := (others => '0');
+	signal line_counter:		unsigned(8 downto 0) := (others => '0');
 
 	
 begin
 	process (NVCAM_CLK)
 	begin
 		if rising_edge(NVCAM_CLK) then
-			if (NVCAM_SYNC = '1') then
+			if (NVCAM_SYNC = '1') then  -- Sync pulse! But what kind?
 				lcd_hsync <= '0';
 				if (NVCAM_VSYNC = '1') then -- New frame
 					line_counter <= (others => '0');
@@ -44,19 +44,17 @@ begin
 					lcd_de <= '1';
 				elsif (NVCAM_VALID = '1') then -- New line of zeros
 					line_counter <= line_counter + 1;
-					if (line_counter >= v_screen) then
+					if (line_counter >= v_screen) then  -- Time for a VSYNC pulse, we are still in the 'black' section from the NVCAM
 						lcd_vsync <= '1';
 					end if;	
 				end if;	
 			else
-				row_counter	<= row_counter + 1;
-				if (row_counter >= h_screen) then
+				row_counter	<= row_counter + 1;  -- Work our way across a horizontal line
+				if (row_counter >= h_screen) then  -- We have reached the end of the screen, time for a HSYNC
 					lcd_hsync <= '1';
-					lcd_de <= '0';	
+					lcd_de <= '0';	-- No more data for this frame is valid, lets just ignore everything from here on...
 				end if;
-				
 			end if;
-
 		end if;
 	end process;
 
